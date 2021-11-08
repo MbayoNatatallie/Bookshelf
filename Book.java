e6126ddfec237d0e0f1835dc53286afe6f281914
Natallie M'bayo <natalliembayo@Natallies-iMac.local>
2021-11-07 21:02:27 -0800

    Add comments for the steps, class, constructors, getter, setter, and toString

--- Book.java
+++ Book.java
@@ -1,11 +1,12 @@
 public class Book {
-  
+  // Instance data are declared private and can be accessed using setter and getter
     private String title;
     private String author;
     private String publisher;
     private String copy_rightdate;
 
-    public Book(String title, String author, String publisher, String copy_rightdate){
+    // Constructor that accept the parameter title, author, publisher, copy_rightdate
+    public Book(String title, String author, String publisher, String copy_rightdate){ 
 
       this.title = title;
       this.author = author;
@@ -13,20 +14,22 @@ public class Book {
       this.copy_rightdate = copy_rightdate;
     }
 
-    public String getTitle(){ // Getter Block for the title
+    // Getter methods returns the values title, author, publisher and copyright, to access the private variable of the book class.
+    public String getTitle(){ 
       return title;
     }
-    public String getAuthor(){ // Getter Block for the author
+    public String getAuthor(){ 
       return author;
     }
-    public String getPublisher(){ // Getter Block for the publisher
+    public String getPublisher(){
       return publisher;
     }
-    public String getCopy_RightDate(){ // Getter Block for copyright date
+    public String getCopy_RightDate(){ 
       return copy_rightdate;
     }
     
 
+    // Setter methods are use to update the values in private variable of the book class. 
     public void setTitle(String title){
 
       this.title = title;
@@ -43,6 +46,8 @@ public class Book {
 
       this.copy_rightdate = copy_rightdate;
     }
+
+    // The toString method is a block template that shows how the strings should be printed when executed
 
     public String toString(){
       return "Title: " + title + "\n" + "Author: " + author + "\n" + "Publisher: " + publisher + "\n" + "Copyright Date: " + copy_rightdate;
--- Bookshelf.java
+++ Bookshelf.java
@@ -1,13 +1,14 @@
-public class Bookshelf {
-    public static void main(String[] args){
+public class Bookshelf { // Driver Class to execute book.java 
+    public static void main(String[] args){ 
 
-       Book book, book1, book2;
-
+      // Create different book variables 
+       Book book, book1, book2; 
         book = new Book ("Left To Tell", "Immaculee Ilibagiza", "Hay House Inc.", "2019-06-14");
         book1 = new Book ("The Dante Chamber", "Matthew Pearl", "Penguin Press", "2018-06-11");
         book2 = new Book ("The Yelllow Belle", "Natallie M'bayo", "Dante House", "2017-05-11");
 
-        System.out.println(book);
+        // Prints out all books in book variables in bookshelf
+        System.out.println(book); 
         System.out.println(book1);
         System.out.println(book2);
 
